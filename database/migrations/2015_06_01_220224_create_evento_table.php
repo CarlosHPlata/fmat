b91074699c3346cda98cd03d4692024374254dbf
Alfonso <alfonsodealba_link@yahoo.com.mx>
2015-06-01 17:11:30 -0500

    Calendario
    Perdón por el atrazo

---
+++ database/migrations/2015_06_01_220224_create_evento_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('eventos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->date('eventDay');
+			$table->string('eventContent');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
